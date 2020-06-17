eca557a664e49bc59b308f7da85da18bff295388
Ammon Rees <Ammonious@gmail.com>
2020-06-17 09:22:15 -0600

    added cardRadius

--- lib/packages/scheme_theme/lib/src/constant_presets.dart
+++ lib/packages/scheme_theme/lib/src/constant_presets.dart
@@ -6,3 +6,6 @@ const Color background = Color(0xFFF2F3F8);
 const Color darkBackground = Color(0xFF0F0F0F);
 const Color nearlyBlack = Color(0xFF213333);
 Color get neuBackground => Color.lerp(Color(0xFFf2f2f2), Colors.black, 0.005);
+
+
+BorderRadius get cardRadius => BorderRadius.circular(16);
