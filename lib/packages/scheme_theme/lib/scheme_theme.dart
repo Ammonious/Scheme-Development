3b1c84efb4f25f6ec5f5ad55544d614f4f5d445d
Ammon Rees <Ammonious@gmail.com>
2020-06-17 00:23:31 -0600

    fixed exports

--- lib/packages/scheme_theme/lib/scheme_theme.dart
+++ lib/packages/scheme_theme/lib/scheme_theme.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 export 'package:scheme_utilities/scheme_utilities.dart';
 export 'src/ui_helper.dart';
 export 'src/models/theme_controller.dart';
+export 'src/picaso/picaso.dart';
 
 
 
